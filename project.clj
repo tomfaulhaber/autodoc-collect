6164e6d35ae56de16081c6e76a5e633e54984ee6
Tom Faulhaber <git_net@infolace.com>
2016-05-24 21:57:28 -0700

    Make this project part of the autodoc group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autodoc-collect "1.1.0"
+(defproject autodoc/autodoc-collect "1.1.0"
   :description "Support library for autodoc"
   :url "http://github.com/tomfaulhaber"
   :license {:name "Eclipse Public License"
