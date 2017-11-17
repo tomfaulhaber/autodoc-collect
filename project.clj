fb10c97ccd33d82141de52933338deb299fa6600
Tom Faulhaber <git_com@infolace.com>
2017-11-16 21:45:42 -0800

    Version 1.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autodoc/autodoc-collect "1.1.4-SNAPSHOT"
+(defproject autodoc/autodoc-collect "1.1.4"
   :description "Support library for autodoc"
   :url "http://github.com/tomfaulhaber"
   :license {:name "Eclipse Public License"
